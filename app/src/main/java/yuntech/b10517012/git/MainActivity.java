c3a3b901f3894f6f061d0983a77506479262dbc2
Lance <li861209@yahoo.com.tw>
2019-05-09 18:47:36 +0800

    testBranch

--- app/src/main/java/yuntech/b10517012/git/MainActivity.java
+++ app/src/main/java/yuntech/b10517012/git/MainActivity.java
@@ -2,14 +2,19 @@ package yuntech.b10517012.git;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
     String test = "Test";
     int tr = 0 ;
+    String test2 = "測試 Reset";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    void testBranch() {
+        Log.d("test","測試分支功能");
     }
 }
