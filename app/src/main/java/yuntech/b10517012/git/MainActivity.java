7505bdd9a122286c3b4ea2608c1275c13f091623
Lance <li861209@yahoo.com.tw>
2019-05-09 18:50:50 +0800

    Branch master add function

--- app/src/main/java/yuntech/b10517012/git/MainActivity.java
+++ app/src/main/java/yuntech/b10517012/git/MainActivity.java
@@ -2,6 +2,7 @@ package yuntech.b10517012.git;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
     String test = "Test";
@@ -11,5 +12,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    void testMaster() {
+        Log.d("test","測試 master 功能");
     }
 }
