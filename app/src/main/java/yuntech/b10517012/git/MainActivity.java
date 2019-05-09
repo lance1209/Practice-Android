6842efea8c5565308ef593d077888183cf56dff9
Lance <li861209@yahoo.com.tw>
2019-05-09 18:34:39 +0800

    First commit

---
+++ app/src/main/java/yuntech/b10517012/git/MainActivity.java
@@ -0,0 +1,13 @@
+package yuntech.b10517012.git;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
